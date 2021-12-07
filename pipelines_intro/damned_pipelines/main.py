db179a4a4bcad983e459ecb4e7765f5057565bb1
Ivan Repilado <ivanrepi@hotmail.com>
2021-12-07 21:41:43 +0100

    pending test it

--- pipelines_intro/damned_pipelines/main.py
+++ pipelines_intro/damned_pipelines/main.py
@@ -7,8 +7,69 @@ import json
 import math
 from modules import module as md
 
-#pendiente poner aquí las variables, si no no va a funcionar no?
+#Before starting the functions, we define all needed variables:
 
+API_TOKEN = "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"
+USERNAME = "ivanrepi"
+BASE_URL = 'https://api.github.com/'
+KEY = 'repos/'
+OWNER = "ih-datapt-mad/"
+REPO = 'dataptmad1121_labs/'
+SEARCH = 'search/issues?q=repo:'+OWNER+REPO+'+type:pr+state:{}'
+PULLS = 'pulls?page={}&per_page=100&state={}'
+COMMITS = 'pulls/{}/commits'
+STATE = 'open'
+
+#We'll need also next lists:
+
+field_list1 = ['number',
+               'title',
+               'state',
+               'created_at',
+               'updated_at',
+               'closed_at',
+               'html_url',
+               'base.repo.full_name',
+               'base.ref',
+               'head.repo.full_name',
+               'head.ref',
+               'head.repo.pushed_at']
+
+field_list2 = ['student_name',
+               'number',
+               'lab_name',
+               'state',
+               'lab_status',
+               'created_at',
+               'updated_at',
+               'closed_at',
+               'html_url',
+               'base.repo.full_name',
+               'base.ref',
+               'head.repo.full_name',
+               'head.ref',
+               'head.repo.pushed_at']
+
+field_sort1 = ['lab_status',
+               'lab_name',
+               'student_name']
+
+field_name1 = ['Student Name',
+               'PR Number',
+               'Lab Name',
+               'PR Status',
+               'Lab Status',
+               'PR Created at',
+               'PR Updated at',
+               'PR Closed at',
+               'PR URL',
+               'base repository',
+               'base',
+               'head repository',
+               'compare',
+               'Pushed at']
+
+               
 def main():
     DF_PULLS = md.get_pulls(BASE_URL, KEY, OWNER, REPO, PULLS, SEARCH, STATE, USERNAME, API_TOKEN, field_list1)
     DF_STATUS = md.df_status(DF_PULLS, BASE_URL, KEY, OWNER, REPO, COMMITS, USERNAME, API_TOKEN, field_list2)
--- pipelines_intro/damned_pipelines/modules/module.py
+++ pipelines_intro/damned_pipelines/modules/module.py
@@ -6,67 +6,6 @@ import re
 import json
 import math
 
-#Before starting the functions, we define all needed variables:
-
-API_TOKEN = "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"
-USERNAME = "ivanrepi"
-BASE_URL = 'https://api.github.com/'
-KEY = 'repos/'
-OWNER = "ih-datapt-mad/"
-REPO = 'dataptmad1121_labs/'
-SEARCH = 'search/issues?q=repo:'+OWNER+REPO+'+type:pr+state:{}'
-PULLS = 'pulls?page={}&per_page=100&state={}'
-COMMITS = 'pulls/{}/commits'
-STATE = 'open'
-
-#We'll need also next lists:
-
-field_list1 = ['number',
-               'title',
-               'state',
-               'created_at',
-               'updated_at',
-               'closed_at',
-               'html_url',
-               'base.repo.full_name',
-               'base.ref',
-               'head.repo.full_name',
-               'head.ref',
-               'head.repo.pushed_at']
-
-field_list2 = ['student_name',
-               'number',
-               'lab_name',
-               'state',
-               'lab_status',
-               'created_at',
-               'updated_at',
-               'closed_at',
-               'html_url',
-               'base.repo.full_name',
-               'base.ref',
-               'head.repo.full_name',
-               'head.ref',
-               'head.repo.pushed_at']
-
-field_sort1 = ['lab_status',
-               'lab_name',
-               'student_name']
-
-field_name1 = ['Student Name',
-               'PR Number',
-               'Lab Name',
-               'PR Status',
-               'Lab Status',
-               'PR Created at',
-               'PR Updated at',
-               'PR Closed at',
-               'PR URL',
-               'base repository',
-               'base',
-               'head repository',
-               'compare',
-               'Pushed at']
 
 
 #FUNCTIONS:
